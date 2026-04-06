b842e91f79cd947a9f555b9e73d8ac0ca94ef897
PULAK-SN <pulak.official22@gmail.com>
2026-04-07 00:49:17 +0530

    get, post, put and delete api

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,15 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+import { ProfilesService } from './profiles.service';
 
 @Controller('profiles')
 export class ProfilesController {
-  // GET /profiles
+  constructor(private profileService: ProfilesService) {}
 
+  // GET /profiles
   @Get()
   findAll() {
-    return [];
+    return this.profileService.findAll();
   }
+
   // GET /profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    this.profileService.findOne(id);
+  }
+
   // POST /profiles
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return this.profileService.create(createProfileDto);
+  }
+
   // PUT /profiles/:id
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProfiledto: UpdateProfileDto) {
+    return this.profileService.update(id, updateProfiledto);
+  }
+
   // DELETE /profiles/:id
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    this.profileService.delete(id);
+  }
 }
